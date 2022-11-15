efcb4f92f7b418df3fe37506bb4a6d6859a66a43
Rafhael Gomes <rafhael@protonmail.com>
2022-11-15 19:32:46 -0300

    feat: ensure SignUpUserUseCase calls hasher.hash with the correct value

--- packages/api/src/application/usecases/sign-up-user.ts
+++ packages/api/src/application/usecases/sign-up-user.ts
@@ -5,16 +5,19 @@ import {
   SignUpUserOutput
 } from '@/domain/usecases/sign-up-user'
 import { UserAlreadyExistsError } from '@/application/errors'
+import { Hasher } from '@/application/contracts'
 
 export class SignUpUserUseCase implements SignUpUser {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly hasher: Hasher
+  ) {}
 
   async execute(input: SignUpUserInput): Promise<SignUpUserOutput> {
-    const { username } = input
+    const { username, password } = input
     const user = await this.userRepository.getUserByUsername(username)
-    if (user) {
-      throw new UserAlreadyExistsError(username)
-    }
+    if (user) throw new UserAlreadyExistsError(username)
+    await this.hasher.hash(password)
     return null as unknown as SignUpUserOutput
   }
 }
