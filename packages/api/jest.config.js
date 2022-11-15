b7e08d653f516bb61a56dcfb2781ba01b9230bc7
Rafhael Gomes <rafhael@protonmail.com>
2022-11-15 16:41:21 -0300

    chore: api boilerplate

---
+++ packages/api/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  testMatch: ['**/*.spec.ts']
+}
