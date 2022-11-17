92ede88e056fa6abad9eb471ee124387121a16bb
Rafhael Gomes <rafhael@protonmail.com>
2022-11-17 20:53:54 -0300

    feat: implement expressRouteAdapter function

---
+++ packages/api/src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,12 @@
+import { Controller } from '@/presentation/contracts'
+import { Request, Response } from 'express'
+
+export function expressRouteAdapter(controller: Controller) {
+  return async (req: Request, res: Response) => {
+    const httpRequest = {
+      body: req.body
+    }
+    const httpResponse = await controller.handle(httpRequest)
+    return res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
