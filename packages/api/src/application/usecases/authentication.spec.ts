364f4e14b51ee6d62163d6e214181955802a6816
Rafhael Gomes <rafhael@protonmail.com>
2022-11-18 17:02:18 -0300

    test: ensure AuthenticationUseCase calls userRepository.getUserByUsername with the correct value

---
+++ packages/api/src/application/usecases/authentication.spec.ts
@@ -0,0 +1,75 @@
+import { faker } from '@faker-js/faker'
+import { User } from '@/domain/entitities/user'
+import {
+  SaveNewUserInput,
+  SaveNewUserOutput,
+  UserRepository
+} from '@/domain/repositories/user-repository'
+import {
+  Authentication,
+  AuthenticationInput
+} from '@/domain/usecases/authentication'
+import { AuthenticationUseCase } from '@/application/usecases/authentication'
+
+function makeFakeUser(): User {
+  return {
+    id: faker.datatype.number(),
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    accountId: faker.datatype.number(),
+    accessToken: null
+  }
+}
+
+const fakeUser = makeFakeUser()
+class UserRepositoryStub implements UserRepository {
+  async getUserByUsername(username: string): Promise<User | null> {
+    return fakeUser
+  }
+
+  async saveNewUser(input: SaveNewUserInput): Promise<SaveNewUserOutput> {
+    return fakeUser
+  }
+
+  async updateAccessToken(
+    userId: number,
+    accessToken: string
+  ): Promise<User | null> {
+    return fakeUser
+  }
+}
+
+interface SutTypes {
+  sut: Authentication
+  userRepositoryStub: UserRepository
+}
+
+function makeSut(): SutTypes {
+  const userRepositoryStub = new UserRepositoryStub()
+  const sut = new AuthenticationUseCase(userRepositoryStub)
+  return {
+    sut,
+    userRepositoryStub
+  }
+}
+
+function makeFakeInput(): AuthenticationInput {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password()
+  }
+}
+
+describe('AuthenticationUseCase', () => {
+  it('should call userRepository.getUserByUsername with the correct value', async () => {
+    const { sut, userRepositoryStub } = makeSut()
+    const getUserByUsernameSpy = jest.spyOn(
+      userRepositoryStub,
+      'getUserByUsername'
+    )
+    const input = makeFakeInput()
+    await sut.execute(input)
+    expect(getUserByUsernameSpy).toHaveBeenCalledTimes(1)
+    expect(getUserByUsernameSpy).toHaveBeenCalledWith(input.username)
+  })
+})
