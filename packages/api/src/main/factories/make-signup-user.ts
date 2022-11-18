92b4f673bef63895c0b6dd0d5b27f9bd1247fdd5
Rafhael Gomes <rafhael@protonmail.com>
2022-11-17 21:32:07 -0300

    refactor: change password max length to 100

--- packages/api/src/main/factories/make-signup-user.ts
+++ packages/api/src/main/factories/make-signup-user.ts
@@ -10,16 +10,14 @@ import { Argon2Adapter } from '@/infra/argon2/argon2-adapter'
 
 export function makeSignUpUserController(): Controller {
   const zodSchema = z.object({
-    body: z.object({
-      username: z.string().min(3).max(20),
-      password: z
-        .string()
-        .min(8)
-        .max(20)
-        .regex(
-          /(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,}).*$/
-        )
-    })
+    username: z.string().min(3).max(20),
+    password: z
+      .string()
+      .min(8)
+      .max(100)
+      .regex(
+        /(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,}).*$/
+      )
   })
   const zodSchemaValidate = new ZodSchemaValidate(zodSchema)
   const postgreSQLUserRepository = new PostgreSQLUserRepository()
