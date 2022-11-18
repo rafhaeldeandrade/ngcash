1edafb3c6d3fb7e3cddc17f14bbf4ef20d07e24c
Rafhael Gomes <rafhael@protonmail.com>
2022-11-17 23:32:12 -0300

    test: ensure PostgreSQLUserRepository.updateAccessToken updates the accessToken correctly

--- packages/api/src/infra/postgresql/user-repository.test.ts
+++ packages/api/src/infra/postgresql/user-repository.test.ts
@@ -76,3 +76,40 @@ describe('PostgreSQLUserRepository.saveNewUser', () => {
     expect(account).not.toBeNull()
   })
 })
+
+describe('PostgreSQLUserRepository.updateAccessToken', () => {
+  beforeAll(async () => {
+    await prismaHelper.connect()
+  })
+
+  afterAll(async () => {
+    await prismaHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    await prismaHelper.prisma.user.deleteMany()
+  })
+
+  it('should update the user with the given accessToken', async () => {
+    const username = faker.internet.userName()
+    const password = faker.internet.password()
+    const account = await prismaHelper.prisma.account.create({
+      data: {
+        balance: 100
+      }
+    })
+    const user = await prismaHelper.prisma.user.create({
+      data: {
+        username,
+        password,
+        accountId: account.id
+      }
+    })
+
+    const sut = new PostgreSQLUserRepository()
+    const accessToken = faker.datatype.uuid()
+    const result = await sut.updateAccessToken(user.id, accessToken)
+
+    expect(result?.accessToken).toBe(accessToken)
+  })
+})
