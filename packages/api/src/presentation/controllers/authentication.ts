cf3f46905f561a4ef9bc61c0cc272fef8010c030
Rafhael Gomes <rafhael@protonmail.com>
2022-11-18 16:23:17 -0300

    feat: ensure AuthenticationController returns 400 if schemaValidate.validate returns an error

--- packages/api/src/presentation/controllers/authentication.ts
+++ packages/api/src/presentation/controllers/authentication.ts
@@ -4,12 +4,14 @@ import {
   HttpResponse,
   SchemaValidate
 } from '@/presentation/contracts'
+import { badRequest } from '@/presentation/helpers/http-helper'
 
 export class AuthenticationController implements Controller {
   constructor(private readonly schemaValidate: SchemaValidate) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.schemaValidate.validate(httpRequest.body)
+    const error = await this.schemaValidate.validate(httpRequest.body)
+    if (error) return badRequest(error)
     return {
       statusCode: 200,
       body: 'ok'
