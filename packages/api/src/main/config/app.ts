0cc882bef37f33390207e7076df48f57a43ed2b5
Rafhael Gomes <rafhael@protonmail.com>
2022-11-17 21:31:30 -0300

    refactor: setup routes

--- packages/api/src/main/config/app.ts
+++ packages/api/src/main/config/app.ts
@@ -1,9 +1,11 @@
 import express, { json, urlencoded } from 'express'
 import cors from 'cors'
+import { setupRoutes } from './setup-routes'
 
 const app = express()
 app.use(json({ strict: false }))
 app.use(urlencoded({ extended: true }))
 app.use(cors())
+setupRoutes(app).catch(console.error)
 
 export { app }
--- packages/api/src/main/server.ts
+++ packages/api/src/main/server.ts
@@ -1,3 +1,11 @@
+import * as dotenv from 'dotenv'
+dotenv.config({
+  path:
+    process.env.NODE_ENV === 'production'
+      ? '.env.production'
+      : '.env.development'
+})
+
 import { app } from '@/main/config/app'
 
 const PORT = 4000
