8bfcafe4176365ebf061899b146203137208fb51
Rafhael Gomes <rafhael@protonmail.com>
2022-11-18 17:12:12 -0300

    feat: ensure AuthenticationUseCase calls hasher.compare with the correct values

--- packages/api/src/application/contracts.ts
+++ packages/api/src/application/contracts.ts
@@ -2,6 +2,10 @@ export interface Hasher {
   hash: (value: string) => Promise<string>
 }
 
+export interface HashComparer {
+  compare: (value: string, hash: string) => Promise<boolean>
+}
+
 export interface Encrypter {
   encrypt: (value: string) => Promise<string>
 }
--- packages/api/src/application/usecases/authentication.ts
+++ packages/api/src/application/usecases/authentication.ts
@@ -5,13 +5,18 @@ import {
   AuthenticationOutput
 } from '@/domain/usecases/authentication'
 import { WrongCredentialsError } from '@/application/errors'
+import { HashComparer } from '@/application/contracts'
 
 export class AuthenticationUseCase implements Authentication {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly hashComparer: HashComparer
+  ) {}
 
   async execute(input: AuthenticationInput): Promise<AuthenticationOutput> {
     const user = await this.userRepository.getUserByUsername(input.username)
     if (!user) throw new WrongCredentialsError()
+    await this.hashComparer.compare(input.password, user.password)
     return {
       accessToken: ''
     }
