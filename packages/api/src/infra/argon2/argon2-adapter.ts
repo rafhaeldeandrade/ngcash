9e7d14773f28d01688d44e583ced0704f84fa645
Rafhael Gomes <rafhael@protonmail.com>
2022-11-18 19:36:46 -0300

    feat: Argon2Adapter now implements HashComparer

--- packages/api/src/infra/argon2/argon2-adapter.ts
+++ packages/api/src/infra/argon2/argon2-adapter.ts
@@ -1,12 +1,16 @@
-import { Hasher } from '@/application/contracts'
+import { HashComparer, Hasher } from '@/application/contracts'
 import argon2 from 'argon2'
 
-export class Argon2Adapter implements Hasher {
+export class Argon2Adapter implements Hasher, HashComparer {
   constructor(
     private readonly argon2Options?: argon2.Options & { raw?: false }
   ) {}
 
   async hash(value: string): Promise<string> {
     return await argon2.hash(value, this.argon2Options)
+  }
+
+  async compare(value: string, hash: string): Promise<boolean> {
+    return await argon2.verify(hash, value, this.argon2Options)
   }
 }
