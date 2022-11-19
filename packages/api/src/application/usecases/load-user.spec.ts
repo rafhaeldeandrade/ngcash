1738a239b0f3a998ba2227474adf4b76ba514b8c
Rafhael Gomes <rafhael@protonmail.com>
2022-11-19 09:23:57 -0300

    test: ensure LoadUserUseCase throws userRepository.findUserByAccessToken throws

--- packages/api/src/application/usecases/load-user.spec.ts
+++ packages/api/src/application/usecases/load-user.spec.ts
@@ -3,6 +3,7 @@ import { LoadUserUseCase } from '@/application/usecases/load-user'
 import { UserRepositoryStub, fakeUser } from '@/utils/test-stubs'
 import { UserRepository } from '@/domain/repositories/user-repository'
 import { faker } from '@faker-js/faker'
+import { UserNotFoundError } from '../errors'
 
 interface SutTypes {
   sut: LoadUser
@@ -30,5 +31,25 @@ describe('LoadUserUseCase', () => {
     await sut.execute(input)
     expect(findUserByTokenSpy).toHaveBeenCalledTimes(1)
     expect(findUserByTokenSpy).toHaveBeenCalledWith(input)
+  })
+
+  it('should throw UserNotFoundError if userRepository.findUserByAccessToken returns null', async () => {
+    const { sut, userRepositoryStub } = makeSut()
+    jest
+      .spyOn(userRepositoryStub, 'findUserByAccessToken')
+      .mockResolvedValueOnce(null)
+    const input = makeFakeInput()
+    const promise = sut.execute(input)
+    await expect(promise).rejects.toThrow(new UserNotFoundError())
+  })
+
+  it('should throw if userRepository.findUserByAccessToken throws', async () => {
+    const { sut, userRepositoryStub } = makeSut()
+    jest
+      .spyOn(userRepositoryStub, 'findUserByAccessToken')
+      .mockRejectedValueOnce(new Error())
+    const input = makeFakeInput()
+    const promise = sut.execute(input)
+    await expect(promise).rejects.toThrow()
   })
 })
