3b2236030494222f98cae96db14718e23f961098
Rafhael Gomes <rafhael@protonmail.com>
2022-11-19 08:07:22 -0300

    feat: ensure AuthenticationMiddleware returns 500 if schemaValidate.validate throws an error

--- packages/api/src/presentation/middlewares/authentication.ts
+++ packages/api/src/presentation/middlewares/authentication.ts
@@ -5,18 +5,23 @@ import {
   SchemaValidate
 } from '@/presentation/contracts'
 import { badRequest } from '@/presentation/helpers/http-helper'
+import { errorAdapter } from '../helpers/error-adapter'
 
 export class AuthenticationMiddleware implements Middleware {
   constructor(private readonly schemaValidate: SchemaValidate) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = await this.schemaValidate.validate(
-      httpRequest.headers.authorization
-    )
-    if (error) return badRequest(error)
-    return {
-      statusCode: 200,
-      body: null
+    try {
+      const error = await this.schemaValidate.validate(
+        httpRequest.headers.authorization
+      )
+      if (error) return badRequest(error)
+      return {
+        statusCode: 200,
+        body: null
+      }
+    } catch (e) {
+      return errorAdapter(e as Error)
     }
   }
 }
