cc4429596eee278e68c82cd1380302385928ece6
Rafhael Gomes <rafhael@protonmail.com>
2022-11-19 09:45:10 -0300

    refactor: change from GET /accounts to get /accounts/balance

--- packages/api/src/main/routes/account.routes.ts
+++ packages/api/src/main/routes/account.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import { expressRouteAdapter } from '@/main/adapters/express-route-adapter'
 import { makeLoadUserAccountBalanceController } from '@/main/factories/make-load-user-account-balance'
+import { expressMiddlewareAdapter } from '@/main/adapters/express-middleware-adapter'
+import { makeAuthenticationMiddleware } from '@/main/factories/make-authentication-middleware'
 
 export default function (router: Router): void {
   router.get(
-    '/accounts',
+    '/accounts/balance',
+    [expressMiddlewareAdapter(makeAuthenticationMiddleware())],
     expressRouteAdapter(makeLoadUserAccountBalanceController())
   )
 }
