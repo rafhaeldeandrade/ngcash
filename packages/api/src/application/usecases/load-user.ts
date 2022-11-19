18cac0fbe0a75f15bda6fce8557b46ded54e7753
Rafhael Gomes <rafhael@protonmail.com>
2022-11-19 09:19:17 -0300

    feat: ensure LoadUserUseCase throws UserNotFoundError if userRepository.findUserByAccessToken returns null

--- packages/api/src/application/usecases/load-user.ts
+++ packages/api/src/application/usecases/load-user.ts
@@ -4,12 +4,14 @@ import {
   LoadUserInput,
   LoadUserOutput
 } from '@/domain/usecases/load-user'
+import { UserNotFoundError } from '@/application/errors'
 
 export class LoadUserUseCase implements LoadUser {
   constructor(private readonly userRepository: UserRepository) {}
 
   async execute(accessToken: LoadUserInput): Promise<LoadUserOutput> {
-    await this.userRepository.findUserByAccessToken(accessToken)
+    const user = await this.userRepository.findUserByAccessToken(accessToken)
+    if (!user) throw new UserNotFoundError()
     return {
       id: 1,
       username: 'any_username',
