5db563b96c070b5f57e995e2eb56adbfccf6979c
Rafhael Gomes <rafhael@protonmail.com>
2022-11-19 22:01:19 -0300

    feat: ensure AddTransactionController returns 400 if schemaValidate.validate returns an error

--- packages/api/src/presentation/controllers/add-transaction.ts
+++ packages/api/src/presentation/controllers/add-transaction.ts
@@ -4,16 +4,18 @@ import {
   HttpResponse,
   SchemaValidate
 } from '../contracts'
+import { badRequest } from '../helpers/http-helper'
 
 export class AddTransactionController implements Controller {
   constructor(private readonly schemaValidate: SchemaValidate) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.schemaValidate.validate({
+    const error = await this.schemaValidate.validate({
       usernameToCashIn: httpRequest.body?.usernameToCashIn,
       amount: httpRequest.body?.amount,
       authAccountId: httpRequest.body?.user?.authAccountId
     })
+    if (error) return badRequest(error)
     return {
       statusCode: 201,
       body: {
