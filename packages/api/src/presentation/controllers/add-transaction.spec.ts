65f56fea2daccbac988ad43892f7e233316b2024
Rafhael Gomes <rafhael@protonmail.com>
2022-11-19 22:00:00 -0300

    test: ensure AddTransactionController calls schemaValidate.validate with correct params

---
+++ packages/api/src/presentation/controllers/add-transaction.spec.ts
@@ -0,0 +1,57 @@
+import { SchemaValidateStub } from '@/utils/test-stubs'
+import { faker } from '@faker-js/faker'
+import { Prisma } from '@prisma/client'
+import { HttpRequest, SchemaValidate } from '../contracts'
+import { AddTransactionController } from './add-transaction'
+
+// const fakeUseCaseOutput = {
+//   from: faker.datatype.uuid(),
+//   to: faker.datatype.uuid(),
+//   amountTransacted: new Prisma.Decimal(faker.datatype.number())
+// }
+// class AddTransactionUseCaseStub implements AddTransaction {
+//   async execute(input: AddTransactionInput): Promise<AddTransactionOutput> {
+//     return fakeUseCaseOutput
+//   }
+// }
+
+interface SutTypes {
+  sut: AddTransactionController
+  schemaValidateStub: SchemaValidate
+}
+
+function makeSut(): SutTypes {
+  const schemaValidateStub = new SchemaValidateStub()
+  const sut = new AddTransactionController(schemaValidateStub)
+  return {
+    sut,
+    schemaValidateStub
+  }
+}
+
+function makeFakeRequest(): HttpRequest {
+  return {
+    body: {
+      usernameToCashIn: faker.internet.userName(),
+      amount: new Prisma.Decimal(faker.datatype.number()),
+      user: {
+        authAccountId: faker.datatype.number()
+      }
+    }
+  }
+}
+
+describe('AddTransactionController', () => {
+  it('should call schemaValidate.validate with correct params', async () => {
+    const { sut, schemaValidateStub } = makeSut()
+    const validateSpy = jest.spyOn(schemaValidateStub, 'validate')
+    const httpRequest = makeFakeRequest()
+    await sut.handle(httpRequest)
+    expect(validateSpy).toHaveBeenCalledTimes(1)
+    expect(validateSpy).toHaveBeenCalledWith({
+      usernameToCashIn: httpRequest.body?.usernameToCashIn,
+      amount: httpRequest.body?.amount,
+      authAccountId: httpRequest.body?.user?.authAccountId
+    })
+  })
+})
