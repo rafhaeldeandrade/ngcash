f7f419faa1aa54e6da105ef67cf3eeb2b3a1bd39
Rafhael Gomes <rafhael@protonmail.com>
2022-11-21 14:47:57 -0300

    feat: provides req.query to controllers

--- packages/api/src/main/adapters/express-middleware-adapter.ts
+++ packages/api/src/main/adapters/express-middleware-adapter.ts
@@ -5,13 +5,14 @@ export function expressMiddlewareAdapter(middleware: Middleware) {
   return async (req: Request, res: Response, next: NextFunction) => {
     const httpRequest = {
       headers: req.headers,
-      body: req.body
+      body: req.body,
+      query: req.query
     }
     const httpResponse = await middleware.handle(httpRequest)
     if (httpResponse.statusCode !== 200 && httpResponse.statusCode !== 201) {
       return res.status(httpResponse.statusCode).json(httpResponse.body)
     }
-    Object.assign(req.body, httpResponse.body)
+    req.body = httpResponse.body
     next()
   }
 }
--- packages/api/src/main/adapters/express-route-adapter.ts
+++ packages/api/src/main/adapters/express-route-adapter.ts
@@ -5,7 +5,8 @@ export function expressRouteAdapter(controller: Controller) {
   return async (req: Request, res: Response) => {
     const httpRequest = {
       body: req.body,
-      params: req.params
+      params: req.params,
+      query: req.query
     }
     const httpResponse = await controller.handle(httpRequest)
     return res.status(httpResponse.statusCode).json(httpResponse.body)
