ac238973caf92e677a4004c79813678a0b684f55
Rafhael Gomes <rafhael@protonmail.com>
2022-11-20 21:39:46 -0300

    feat: ensure LoadTransactionsController returns 500 if schemaValidate.validate throws an error

--- packages/api/src/presentation/controllers/load-transactions.ts
+++ packages/api/src/presentation/controllers/load-transactions.ts
@@ -5,17 +5,22 @@ import {
   SchemaValidate
 } from '@/presentation/contracts'
 import { badRequest } from '@/presentation/helpers/http-helper'
+import { errorAdapter } from '../helpers/error-adapter'
 
 export class LoadTransactionsController implements Controller {
   constructor(private readonly schemaValidate: SchemaValidate) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { query } = httpRequest
-    const error = await this.schemaValidate.validate(query)
-    if (error) return badRequest(error)
-    return {
-      statusCode: 200,
-      body: {}
+    try {
+      const { query } = httpRequest
+      const error = await this.schemaValidate.validate(query)
+      if (error) return badRequest(error)
+      return {
+        statusCode: 200,
+        body: {}
+      }
+    } catch (e) {
+      return errorAdapter(e as Error)
     }
   }
 }
