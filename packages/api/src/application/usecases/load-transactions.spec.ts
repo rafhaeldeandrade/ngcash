09d9df79715c7921d6209f312a465029ec531d5a
Rafhael Gomes <rafhael@protonmail.com>
2022-11-20 22:39:43 -0300

    test: ensure LoadTransactionsUseCase calls transactionRepository.findAll with the correct values when transactionType is all

---
+++ packages/api/src/application/usecases/load-transactions.spec.ts
@@ -0,0 +1,49 @@
+import { faker } from '@faker-js/faker'
+import {
+  LoadTransactions,
+  LoadTransactionsInput
+} from '@/domain/usecases/load-transactions'
+import { LoadTransactionsUseCase } from '@/application/usecases/load-transactions'
+import { TransactionRepository } from '@/domain/repositories/transaction-repository'
+import { TransactionRepositoryStub } from '@/utils/test-stubs'
+
+interface SutTypes {
+  sut: LoadTransactions
+  transactionRepositoryStub: TransactionRepository
+}
+
+function makeSut(): SutTypes {
+  const transactionRepositoryStub = new TransactionRepositoryStub()
+  const sut = new LoadTransactionsUseCase(transactionRepositoryStub)
+  return {
+    sut,
+    transactionRepositoryStub
+  }
+}
+
+function makeFakeInput(): LoadTransactionsInput {
+  return {
+    accountId: faker.datatype.number(),
+    page: faker.datatype.number(),
+    transactionType: faker.helpers.arrayElement(['cashIn', 'cashOut']),
+    date: faker.date.past()
+  }
+}
+
+describe('LoadTransactionsUseCase', () => {
+  it('should call transactionRepository.findAll with the correct values when transactionType is all', async () => {
+    const { sut, transactionRepositoryStub } = makeSut()
+    const findAllSpy = jest.spyOn(transactionRepositoryStub, 'findAll')
+    const input = makeFakeInput()
+    input.transactionType = 'all'
+    await sut.execute(input)
+    expect(findAllSpy).toHaveBeenCalledTimes(1)
+    expect(findAllSpy).toHaveBeenCalledWith({
+      createdAt: input.date,
+      debitedAccountId: input.accountId,
+      creditedAccountId: input.accountId,
+      skip: input.page - 1 * 20,
+      take: 20
+    })
+  })
+})
