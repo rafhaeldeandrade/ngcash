8ad4edf5ab60d02aaf44fb78844fa755ec40ae10
Rafhael Gomes <rafhael@protonmail.com>
2022-11-20 23:12:37 -0300

    feat: implement GET /transactions route

--- packages/api/src/main/routes/transaction.routes.ts
+++ packages/api/src/main/routes/transaction.routes.ts
@@ -3,11 +3,17 @@ import { expressRouteAdapter } from '@/main/adapters/express-route-adapter'
 import { makeAddTransactionController } from '@/main/factories/make-add-transaction'
 import { expressMiddlewareAdapter } from '@/main/adapters/express-middleware-adapter'
 import { makeAuthenticationMiddleware } from '@/main/factories/make-authentication-middleware'
+import { makeLoadTransactionsController } from '@/main/factories/make-load-transactions'
 
 export default function (router: Router): void {
   router.post(
     '/transactions',
     [expressMiddlewareAdapter(makeAuthenticationMiddleware())],
     expressRouteAdapter(makeAddTransactionController())
+  )
+  router.get(
+    '/transactions',
+    [expressMiddlewareAdapter(makeAuthenticationMiddleware())],
+    expressRouteAdapter(makeLoadTransactionsController())
   )
 }
